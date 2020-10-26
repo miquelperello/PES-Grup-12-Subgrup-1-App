edfc0c22b6c3be7b7d4bad470eadfc564fc9ddd5
miquelperello <miquel.perello.rodriguez@gmail.com>
2020-10-26 20:58:01 +0100

    Disseny seleccionar Esdeveniment part 1

--- app/src/main/java/com/example/myapplication/MyAdapter.kt
+++ app/src/main/java/com/example/myapplication/MyAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.row.view.*
 
@@ -27,6 +28,17 @@ class MyAdapter(val arrayList: ArrayList<Model>, val context: Context) : Recycle
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bindItems(arrayList[position])
+
+        holder.itemView.setOnClickListener{
+
+            if(position==0) {
+                Toast.makeText(
+                        context,
+                        "You clicked over Newsfeed",
+                        Toast.LENGTH_LONG
+                ).show()
+            }
+        }
     }
 
     override fun getItemCount(): Int {
