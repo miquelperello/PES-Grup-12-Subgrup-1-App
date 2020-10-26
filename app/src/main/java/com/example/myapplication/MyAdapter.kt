c497d3aeb75fc5cd6cc36b9df694c156954a1d34
miquelperello <miquel.perello.rodriguez@gmail.com>
2020-10-26 16:08:28 +0100

    Classe MyAdapter amb error.

---
+++ app/src/main/java/com/example/myapplication/MyAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter(val arrayList: ArrayList<Model>, val context: Context) :
+        RecyclerView.Adapter<MyAdapter.ViewHolder>(){
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+        fun bindItems(model: Model){
+
+            itemView.title.text = model.title
+            itemView.descriptionTv.text = model.des
+            itemView.imageIv.setImageResource(model.image)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.row, parent, false)
+
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindItems(arrayList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return arrayList.size
+    }
+}
