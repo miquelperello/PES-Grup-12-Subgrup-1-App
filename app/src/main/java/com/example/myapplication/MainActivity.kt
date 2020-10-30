fef2af8e875ba03a338dcbc01b61e23bca382432
aleix.vila.berenguel <aleix.vila.berenguel@est.fib.upc.edu>
2020-10-30 20:12:57 +0100

    Histories d'usuari #41,#106, es visualitzen tots els esdeveniments, i l'informació corresponent a cadascun.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -60,42 +60,6 @@ class MainActivity : AppCompatActivity() {
             }
         }, { error -> error.printStackTrace() })
         requestQueue?.add(request)
-
-/*
-        val arrayList = ArrayList<Model>()
-        arrayList.add(Model("Event1", "Tipus1", R.mipmap.ic_launcher))
-        arrayList.add(Model("Event2", "Tipus2", R.mipmap.ic_launcher))
-        arrayList.add(Model("Event3", "Tipus3", R.mipmap.ic_launcher))
-        arrayList.add(Model("Event4", "Tipus4", R.mipmap.ic_launcher))
-        arrayList.add(Model("Event5", "Tipus5", R.mipmap.ic_launcher))
-        arrayList.add(Model("Event6", "Tipus6", R.mipmap.ic_launcher))
-        arrayList.add(Model("Event7", "Tipus7", R.mipmap.ic_launcher))
-        arrayList.add(Model("Event8", "Tipus8", R.mipmap.ic_launcher))
-        arrayList.add(Model("Event9", "Tipus9", R.mipmap.ic_launcher))
-        */
-
-
-
-        /*findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()*/
-        }
-
-
-    /*override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }*/
-
-    /*override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }*/
+    }
 }
 
