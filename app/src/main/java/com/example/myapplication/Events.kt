8f2a238595d678bbb71b237952040b954a640642
miquelperello <miquel.perello.rodriguez@gmail.com>
2020-11-09 19:41:08 +0100

    Navigation Drawer - Versió amb fragments.

--- app/src/main/java/com/example/myapplication/Events.kt
+++ app/src/main/java/com/example/myapplication/Events.kt
@@ -1,10 +1,26 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_events.*
+import kotlinx.android.synthetic.main.fragment_events.drawerLayout
+import kotlinx.android.synthetic.main.fragment_events.recyclerView
+import org.json.JSONException
+
+
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
@@ -19,6 +35,7 @@ class Events : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var requestQueue: RequestQueue? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +43,43 @@ class Events : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+       val url = "https://securevent.herokuapp.com/events"
+        requestQueue = Volley.newRequestQueue(getActivity()?.getApplicationContext())
+        val arrayList = ArrayList<Model>()
+
+       val request = JsonArrayRequest(Request.Method.GET, url, null, { response ->
+           try {
+               for (i in 0 until response.length()) {
+                   val event = response.getJSONObject(i)
+                   arrayList.add(
+                           Model(
+                                   event.getString("name"),
+                                   event.getString("street"),
+                                   R.drawable.icon,
+                                   event.getString("street"),
+                                   event.getString("date"),
+                                   event.getString("hourIni"),
+                                   event.getString("minPrice"),
+                                   event.getString("maxPrice")
+                           )
+                   )
+               }
+
+               val myAdapter = (getActivity()?.getApplicationContext()?.let { MyAdapter(arrayList, it) })
+
+               recyclerView.layoutManager = LinearLayoutManager(activity)
+               recyclerView.adapter = myAdapter
+
+
+           } catch (e: JSONException) {
+               e.printStackTrace()
+           }
+       }, { error -> error.printStackTrace() })
+        requestQueue?.add(request)
+
+
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -33,6 +87,24 @@ class Events : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_events, container, false)
     }
+
+    class MyDrawerLayout : DrawerLayout {
+        constructor(context: Context?) : super(context!!) {}
+        constructor(context: Context?, attrs: AttributeSet?) : super(context!!, attrs) {}
+        constructor(context: Context?, attrs: AttributeSet?, defStyle: Int) : super(context!!, attrs, defStyle) {}
+
+        override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+            var widthMeasureSpec = widthMeasureSpec
+            var heightMeasureSpec = heightMeasureSpec
+            widthMeasureSpec = MeasureSpec.makeMeasureSpec(
+                    MeasureSpec.getSize(widthMeasureSpec), MeasureSpec.EXACTLY)
+            heightMeasureSpec = MeasureSpec.makeMeasureSpec(
+                    MeasureSpec.getSize(heightMeasureSpec), MeasureSpec.EXACTLY)
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        }
+    }
+
+
 
     companion object {
         /**
@@ -52,4 +124,7 @@ class Events : Fragment() {
                     }
                 }
     }
+
+
+
 }
