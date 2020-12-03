80ad53f9baf646dbb7bc8b5554e33a70a9785017
Alex Catalan <alexcatmu@gmail.com>
2020-12-03 23:33:01 +0100

    buy with paypal API and add keys in local.properties

--- app/src/main/java/com/pes/securevent/BuyPaypal.kt
+++ app/src/main/java/com/pes/securevent/BuyPaypal.kt
@@ -1,10 +1,24 @@
 package com.pes.securevent
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.paypal.android.sdk.payments.*
+import org.json.JSONException
+import java.math.BigDecimal
+
 
 class BuyPaypal : AppCompatActivity() {
+    val PAYPAL_REQUEST_CODE = 123
+
+    val config = PayPalConfiguration()
+        .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
+        .clientId(BuildConfig.keyPayPal)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_buy_paypal)
@@ -14,5 +28,70 @@ class BuyPaypal : AppCompatActivity() {
         actionBar.setDisplayShowHomeEnabled(true)
 
         actionBar.title = resources.getString(R.string.PayPal)
+
+        val intent = Intent(this, PayPalService::class.java)
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+        startService(intent);
+    }
+
+    override fun onDestroy() {
+        stopService(Intent(this, PayPalService::class.java))
+        super.onDestroy()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        //If the result is from paypal
+        if (requestCode == PAYPAL_REQUEST_CODE) {
+
+            //If the result is OK i.e. user has not canceled the payment
+            if (resultCode == RESULT_OK) {
+                //Getting the payment confirmation
+                val confirm: PaymentConfirmation? =
+                    data?.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION)
+
+                //if confirmation is not null
+                if (confirm != null) {
+                    try {
+                        //Getting the payment details
+                        val paymentDetails = confirm.toJSONObject().toString(4)
+                        Log.i("paymentExample", paymentDetails)
+                        Snackbar.make(findViewById(android.R.id.content), getResources().getString(R.string.MessageInscripcioEvent), Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show()
+                    } catch (e: JSONException) {
+                        Log.e("paymentExample", "an extremely unlikely failure occurred: ", e)
+                    }
+                }
+            } else if (resultCode == RESULT_CANCELED) {
+                Log.i("paymentExample", "The user canceled.")
+            } else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID) {
+                Log.i(
+                    "paymentExample",
+                    "An invalid Payment or PayPalConfiguration was submitted. Please see the docs."
+                )
+            }
+        }
+    }
+
+    fun buy(view: View) {
+        //Creating a paypalpayment
+        val payment = PayPalPayment(
+            BigDecimal(50), "USD", "Simplified Coding Fee",
+            PayPalPayment.PAYMENT_INTENT_SALE
+        )
+
+        //Creating Paypal Payment activity intent
+        val intent = Intent(this, PaymentActivity::class.java)
+
+        //putting the paypal configuration to the intent
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config)
+
+        //Puting paypal payment to the intent
+        intent.putExtra(PaymentActivity.EXTRA_PAYMENT, payment)
+
+        //Starting the intent activity for result
+        //the request code will be used on the method onActivityResult
+        startActivityForResult(intent, PAYPAL_REQUEST_CODE)
+        println("poh aqui tamoh")
     }
 }
