98225ea57cd353286d0b8529d8b2c14d44231061
miquelperello <miquel.perello.rodriguez@gmail.com>
2020-12-09 21:17:43 +0100

    Crides POST, visualització i Google Pay funcionant.

--- app/src/main/java/com/pes/securevent/BuyPaypal.kt
+++ app/src/main/java/com/pes/securevent/BuyPaypal.kt
@@ -7,29 +7,38 @@ import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.GridView
+import android.widget.Toast
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.DefaultRetryPolicy
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
-import com.google.android.material.snackbar.Snackbar
-import com.paypal.android.sdk.payments.*
-import org.json.JSONArray
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.wallet.*
 import org.json.JSONException
 import org.json.JSONObject
 import java.math.BigDecimal
+import kotlinx.android.synthetic.main.activity_buy_paypal.*
 
 
 class BuyPaypal : AppCompatActivity() {
-    val PAYPAL_REQUEST_CODE = 123
+    val LOAD_PAYMENT_DATA_REQUEST_CODE = 123
 
-    val config = PayPalConfiguration()
-        .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
-        .clientId(BuildConfig.keyPayPal)
+
+
+    private lateinit var paymentsClient: PaymentsClient
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_buy_paypal)
+
+
+        // Initialize a Google Pay API client for an environment suitable for testing.
+        // It's recommended to create the PaymentsClient object inside of the onCreate method.
+        paymentsClient = PaymentsUtil.createPaymentsClient(this)
+        possiblyShowGooglePayButton()
+
 
         val actionBar : ActionBar? = supportActionBar
         actionBar!!.setDisplayHomeAsUpEnabled(true)
@@ -49,13 +58,14 @@ class BuyPaypal : AppCompatActivity() {
 
         // GET THE MATRIX DIMENSIONS
         val rows = prematrix!!.size // Utilizar el put.extra para conseguir filas y columnas de la room
-        var columns = prematrix.get(0).filter{it!= '\t'}.count()
+        var columns = prematrix.get(0).split('\t').count()
+        //var columns = prematrix.get(0).filter{it!= '\t'}.count()
 
 
-        var sala = ArrayList<String>()
+        var sala = ArrayList<ArrayList<String>>()
 
-        for (i in 0 until columns-1){
-            sala.add(prematrix.get(i).filter{it!= '\t'})
+        for (i in 0 until rows ){
+            sala.add(prematrix.get(i).split('\t') as ArrayList<String>)
         }
 
 
@@ -70,12 +80,12 @@ class BuyPaypal : AppCompatActivity() {
         for (i in 0 until rows) {
             for (j in 0 until columns) {
 
-                if (sala.get(i).get(j) == 'T')
+                if (sala.get(i).get(j) == "T")
                     salaList.add(Seat(i, j, 'T'))
-                else if (sala.get(i).get(j) == 'F')
-                    salaList.add(Seat(i, j, 'F'))
-                else
+                else if (sala.get(i).get(j) == "F")
                     salaList.add(Seat(i, j, 'C'))
+                else
+                    salaList.add(Seat(i, j, 'F'))
 
 
             }
@@ -87,53 +97,105 @@ class BuyPaypal : AppCompatActivity() {
         // ATTACH THE ADAPTER TO GRID
         grid.adapter = adapter
 
-        val intent = Intent(this, PayPalService::class.java)
-        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
-        startService(intent);
+
     }
 
-    override fun onDestroy() {
-        stopService(Intent(this, PayPalService::class.java))
-        super.onDestroy()
-    }
+    private fun possiblyShowGooglePayButton() {
+        val isReadyToPayJson = PaymentsUtil.isReadyToPayRequest() ?: return
+        val request = IsReadyToPayRequest.fromJson(isReadyToPayJson.toString()) ?: return
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        //If the result is from paypal
-        if (requestCode == PAYPAL_REQUEST_CODE) {
-
-            //If the result is OK i.e. user has not canceled the payment
-            if (resultCode == RESULT_OK) {
-                //Getting the payment confirmation
-                val confirm: PaymentConfirmation? =
-                    data?.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION)
-
-                //if confirmation is not null
-                if (confirm != null) {
-                    try {
-                        //Getting the payment details
-                        val paymentDetails = confirm.toJSONObject().toString(4)
-                        Log.i("paymentExample", paymentDetails)
-                        Snackbar.make(
-                                findViewById(android.R.id.content),
-                                getResources().getString(R.string.MessageInscripcioEvent),
-                                Snackbar.LENGTH_LONG
-                        )
-                            .setAction("Action", null).show()
-                    } catch (e: JSONException) {
-                        Log.e("paymentExample", "an extremely unlikely failure occurred: ", e)
-                    }
-                }
-            } else if (resultCode == RESULT_CANCELED) {
-                Log.i("paymentExample", "The user canceled.")
-            } else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID) {
-                Log.i(
-                        "paymentExample",
-                        "An invalid Payment or PayPalConfiguration was submitted. Please see the docs."
-                )
+        // The call to isReadyToPay is asynchronous and returns a Task. We need to provide an
+        // OnCompleteListener to be triggered when the result of the call is known.
+        val task = paymentsClient.isReadyToPay(request)
+        task.addOnCompleteListener { completedTask ->
+            try {
+                completedTask.getResult(ApiException::class.java)?.let(::setGooglePayAvailable)
+            } catch (exception: ApiException) {
+                // Process error
+                Log.w("isReadyToPay failed", exception)
             }
         }
+
     }
+
+    private fun setGooglePayAvailable(available: Boolean) {
+        if (available) {
+            googlePayButton.visibility = View.VISIBLE
+        } else {
+            Toast.makeText(
+                this,
+                "Unfortunately, Google Pay is not available on this device",
+                Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
+
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            // Value passed in AutoResolveHelper
+            LOAD_PAYMENT_DATA_REQUEST_CODE -> {
+                when (resultCode) {
+                    RESULT_OK ->
+                        data?.let { intent ->
+                            PaymentData.getFromIntent(intent)?.let(::handlePaymentSuccess)
+                        }!!
+
+                    RESULT_CANCELED -> {
+                        // The user cancelled the payment attempt
+                    }
+
+                    AutoResolveHelper.RESULT_ERROR -> {
+                        AutoResolveHelper.getStatusFromIntent(data)?.let {
+                            handleError(it.statusCode)
+                        }!!
+                    }
+                }
+
+                // Re-enables the Google Pay payment button.
+                googlePayButton.isClickable = true
+            }
+        }
+
+
+
+
+
+
+    }
+
+    private fun handlePaymentSuccess(paymentData: PaymentData) {
+        val paymentInformation = paymentData.toJson() ?: return
+
+        try {
+            // Token will be null if PaymentDataRequest was not constructed using fromJson(String).
+            val paymentMethodData = JSONObject(paymentInformation).getJSONObject("paymentMethodData")
+            val billingName = paymentMethodData.getJSONObject("info")
+                .getJSONObject("billingAddress").getString("name")
+            Log.d("BillingName", billingName)
+
+            //Toast.makeText(this, getString(R.string.payments_show_name, billingName), Toast.LENGTH_LONG).show()
+
+            // Logging token string.
+            Log.d("GooglePaymentToken", paymentMethodData
+                .getJSONObject("tokenizationData")
+                .getString("token"))
+
+        } catch (e: JSONException) {
+            Log.e("handlePaymentSuccess", "Error: " + e.toString())
+        }
+
+    }
+    private fun handleError(statusCode: Int) {
+        Log.w("loadPaymentData failed", String.format("Error code: %d", statusCode))
+    }
+
+
+
 
     fun increment(view: View) {
         val edit_text_tickets = findViewById<EditText>(R.id.numTickets)
@@ -151,45 +213,47 @@ class BuyPaypal : AppCompatActivity() {
             edit_text_tickets.setText((numTickets - 1).toString())
     }
 
+
+
+
     fun buy(view: View) {
 
-        val edit_text_tickets = findViewById<EditText>(R.id.numTickets)
-        val numTickets = edit_text_tickets.text.toString().toInt()
 
-        if (numTickets > 0) {
-
-            // Get extras in the Esdeveniment.kt
+            //val isReadyToPayJson = PaymentsUtil.isReadyToPayRequest() ?: return
+            //val request = IsReadyToPayRequest.fromJson(isReadyToPayJson.toString()) ?: return
             val extras = intent.extras
 
-            //Creating a paypalpayment
-            val payment = PayPalPayment(
-                    BigDecimal(extras?.getString("qtt")).toInt().toBigDecimal() * numTickets.toBigDecimal(),
-                    "USD",
-                    "Simplified Coding Fee",
-                    PayPalPayment.PAYMENT_INTENT_SALE
-            )
+            val edit_text_tickets = findViewById<EditText>(R.id.numTickets)
+            val numTickets = edit_text_tickets.text.toString().toInt()
+        if (numTickets > 0) {
+            val paymentDataRequestJson = PaymentsUtil.getPaymentDataRequest(0)
+            if (paymentDataRequestJson == null) {
+                Log.e("RequestPayment", "Can't fetch payment data request")
+                return
+            }
+            val request = PaymentDataRequest.fromJson(paymentDataRequestJson.toString())
 
-            //Creating Paypal Payment activity intent
-            val intent = Intent(this, PaymentActivity::class.java)
-
-            //putting the paypal configuration to the intent
-            intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config)
-
-            //Puting paypal payment to the intent
-            intent.putExtra(PaymentActivity.EXTRA_PAYMENT, payment)
-
+            // Since loadPaymentData may show the UI asking the user to select a payment method, we use
+            // AutoResolveHelper to wait for the user interacting with it. Once completed,
+            // onActivityResult will be called with the result.
+            if (request != null) {
+                AutoResolveHelper.resolveTask(
+                        paymentsClient.loadPaymentData(request), this, LOAD_PAYMENT_DATA_REQUEST_CODE)
+            }
             //Starting the intent activity for result
             //the request code will be used on the method onActivityResult
 
-            startActivityForResult(intent, PAYPAL_REQUEST_CODE)
+            postEvent(extras?.getString("eventID"), numTickets.toString())
 
-            postEvent(extras?.getString("eventID"))
         }
+
     }
 
-    fun postEvent(eventID: String?) {
+    fun postEvent(eventID: String?, numTickets: String) {
 
-        val url = "https://securevent.herokuapp.com/reservations"
+
+        val url = "https://securevent.herokuapp.com/reservations?n=$numTickets"
+
 
         //creem objecte JSON per fer la crida POST
         val params = JSONObject()
